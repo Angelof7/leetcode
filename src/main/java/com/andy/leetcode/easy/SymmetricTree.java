24748112dd4ce562a8dc7de46292c17b1f05e04b
gaochao07 <cgao886@163.com>
2019-05-22 21:42:40 +0800

    101. Symmetric Tree

---
+++ src/main/java/com/andy/leetcode/easy/SymmetricTree.java
@@ -0,0 +1,91 @@
+package com.andy.leetcode.easy;
+
+import com.andy.leetcode.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by andy on 2019/5/22.
+ */
+public class SymmetricTree {
+    /**
+     * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+     * <p>
+     * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+     * <p>
+     * 1
+     * / \
+     * 2   2
+     * / \ / \
+     * 3  4 4  3
+     * <p>
+     * <p>
+     * But the following [1,2,2,null,3,null,3] is not:
+     * <p>
+     * 1
+     * / \
+     * 2   2
+     * \   \
+     * 3    3
+     *
+     * @seehttps://leetcode.com/problems/symmetric-tree/
+     */
+    public boolean isSymmetric(TreeNode root) {
+//        return isMirror(root, root);
+        return isMirrorIterative(root);
+    }
+
+    private boolean isMirrorIterative(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        if (t1.val != t2.val) {
+            return false;
+        }
+        return isMirror(t1.left, t2.right)
+                && isMirror(t1.right, t2.left);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println(new SymmetricTree().isSymmetric(root));
+    }
+}
